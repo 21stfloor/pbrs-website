682f6f9ba719ab11286bbec7da33bcf8b1505367
John Erick Arzaga <arzagaerick11@gmail.com>
2024-06-01 16:31:34 +0800

    implemented admin crud with filter and search

--- pbrs/pbrs_app/admin.py
+++ pbrs/pbrs_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import HouseListing
 
 # Register your models here.
+class HouseListingAdmin(admin.ModelAdmin):
+    list_display = ('house_name', 'total_contract_price', 'type_of_house', 'address', 'location_coordinates', 'contact_agent')
+    search_fields = ('house_name', 'type_of_house', 'address', 'contact_agent')
+    list_filter = ('type_of_house', 'contact_agent')
+    
+admin.site.register(HouseListing, HouseListingAdmin)
--- pbrs/pbrs_app/models.py
+++ pbrs/pbrs_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class HouseListing(models.Model):
+    house_name = models.CharField(max_length=100)
+    total_contract_price = models.DecimalField(max_digits=10, decimal_places=2)
+    type_of_house = models.CharField(max_length=100)
+    house_images = models.ImageField(upload_to='house_images/')
+    address = models.CharField(max_length=100)
+    location_coordinates = models.CharField(max_length=100)
+    contact_agent = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.house_name
