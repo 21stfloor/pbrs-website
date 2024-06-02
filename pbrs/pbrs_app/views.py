25e63d808311e64cb44c052480e51bae9b3a4476
CJ Rosario <cjricegod@gmail.com>
2024-06-02 15:25:49 +0800

    created listings page to search from name, type and agent

--- pbrs/pbrs_app/views.py
+++ pbrs/pbrs_app/views.py
@@ -1,6 +1,18 @@
+from django.db.models import Q
 from django.shortcuts import render
+from.models import HouseListing
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def listings(request):
+    query = request.GET.get('query', '')
+    if query:
+        house_listings = HouseListing.objects.filter(
+            Q(house_name__icontains=query) | Q(contact_agent__icontains=query) | Q(type_of_house__icontains=query)
+        )
+    else:
+        house_listings = HouseListing.objects.all()
+    return render(request, 'listings.html', {'house_listings': house_listings})
